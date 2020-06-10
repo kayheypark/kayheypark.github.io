780ce97a834b940708200b84e5769251912a160b
Park Gyeong Ho <59360980+kayheypark@users.noreply.github.com>
2020-06-10 18:37:10 +0900

    0610

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 /* main.js */
+
+$(function () {
+    //a 누를때 자동스크롤 방지
+    $("a").click(function(e){
+        e.preventDefault();
+    });
+
+    //상단에서 내려오는 .gnb
+    var flag = false;
+    $(".btn_hamberger").click(function(){
+
+        if(flag == 0){//열음
+            $("#gnb").addClass("on");
+            $(".btn_hamberger i").removeClass("fas fa-bars");
+            $(".btn_hamberger i").addClass("fas fa-times");
+            flag = true;
+        } else {//닫음
+            $("#gnb").removeClass("on");
+            $(".btn_hamberger i").removeClass("fas fa-times");
+            $(".btn_hamberger i").addClass("fas fa-bars");
+            flag = false;
+        }
+
+    });
+
+    
+}); // DocReady Close
+
+//<i class="fas fa-times"></i>
