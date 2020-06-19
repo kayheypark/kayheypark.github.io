0ce1aafa1289add675eef78c9f9bf500b9be6111
Park Gyeong Ho <59360980+kayheypark@users.noreply.github.com>
2020-06-19 17:56:24 +0900

    back btn

--- js/samsung_sub.js
+++ js/samsung_sub.js
@@ -1,12 +1,40 @@
 /* samsung_sub.js */
 
+
 $(function () {
 
-    //
-    $("a").click(function(e){
-        e.preventDefault();
-    });
+
+
     
+    
+    //
+    // $("").click(function(e){
+    //     e.preventDefault();
+    // });
+   //GNB
+   function gnbLstClk(){ // 리스트 클릭시 버튼 모양을 바꿔준다.
+
+    if(flag == 0){//열음
+    $(".btn_hamberger i").removeClass("fas fa-bars");
+    $(".btn_hamberger i").addClass("fas fa-times");
+    flag = true;
+} else {//닫음
+    $(".btn_hamberger i").removeClass("fas fa-times");
+    $(".btn_hamberger i").addClass("fas fa-bars");
+    flag = false;
+}
+
+}
+
+
+
+
+
+
+
+
+
+
     //상단에서 내려오는 .gnb
     var flag = false;
     $(".btn_hamberger").click(function (){
@@ -28,6 +56,7 @@ $(function () {
 
     $(window).scroll(function(){
         var scrollTop = $(this).scrollTop() ;
+
         if(scrollTop >= 941 ){
             $("header").addClass("black");
         } else {
@@ -36,5 +65,14 @@ $(function () {
         console.log(scrollTop);
 
     });
+
+
+    // $("header").click(function(){
+    //     alert("뒤로가기");
+	// 	window.history.back();
+
+	// });
+
+
 
 });
